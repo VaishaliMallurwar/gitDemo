3d944a1025f57df8281582004260e0f2c7f635f2
Mangesh K <vaishali.mallurwar@gmail.com>
2025-03-02 03:24:30 +0530

    i have added code registartion page

---
+++ src/registration/Registration.java
@@ -0,0 +1,13 @@
+package registration;
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int x ,y ,z;
+	      System.out.println("hello git how are you");
+
+	}
+
+}
