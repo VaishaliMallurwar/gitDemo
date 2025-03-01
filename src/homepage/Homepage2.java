3ecbddfa8414f8a508f1f05c5b4cbe3c1c95e008
Mangesh K <vaishali.mallurwar@gmail.com>
2025-03-02 02:38:00 +0530

    i have added two java file

---
+++ src/homepage/Homepage2.java
@@ -0,0 +1,12 @@
+package homepage;
+
+public class Homepage2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a ,b ,c;
+	      System.out.println("hello git how are you");
+	}
+
+}
